3ae384f8fd8293e0e6bc0a3fd0d7336238d288b3
me <alexander.harding@gmail.com>
2021-06-11 16:11:21 -0400

    awefawef

--- configure.sh
+++ configure.sh
@@ -2,15 +2,13 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y samba samba-common-bin smbclient cifs-utils
 
-mkdir ~/ooo
-mkdir ~/ooo/CrystalDimension
-mkdir ~/ooo/LumpySpace
+mkdir /home/pi/ooo
+mkdir /home/pi/ooo/CrystalDimension
+mkdir /home/pi/ooo/LumpySpace
 
-touch ~/.smb
-echo user=alex >> ~/.smb
-echo password=bumjy >> ~/.smb
-
-sudo su
+touch /home/pi/.smb
+echo user=alex >> /home/pi/.smb
+echo password=bumjy >> /home/pi/.smb
 
 echo //ooo.local/CrystalDimension /home/pi/ooo/CrystalDimension	cifs uid=0,credentials=/home/pi/.smb,iocharset=utf8,noperm 0 0 >> /etc/fstab
 echo //ooo.local/LumpySpace /home/pi/ooo/LumpySpace cifs uid=0,credentials=/home/pi/.smb,iocharset=utf8,noperm 0 0 >> /etc/fstab
