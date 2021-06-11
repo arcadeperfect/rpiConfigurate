6af5838359c86af9c8e61414de839ae0b9c1bf78
me <alexander.harding@gmail.com>
2021-06-11 16:01:28 -0400

    added junk

--- configure.sh
+++ configure.sh
@@ -8,3 +8,10 @@ mkdir ~/ooo/LumpySpace
 
 touch ~/.smb
 echo user=alex >> ~/.smb
+echo password=bumjy >> ~/.smb
+
+
+sudo su
+
+echo //ooo.local/CrystalDimension /home/pi/ooo/CrystalDimension	cifs uid=0,credentials=/home/pi/.smb,iocharset=utf8,noperm 0 0 >> /etc/fstab
+echo //ooo.local/LumpySpace /home/pi/ooo/LumpySpace cifs uid=0,credentials=/home/pi/.smb,iocharset=utf8,noperm 0 0 >> /etc/fstab
